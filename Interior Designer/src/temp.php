ec6c110d5ea0c985ce072e1ba2d2207f9f5aa232
Shubham Barthwal <shbhmbarthwal94@gmail.com>
2017-04-01 18:08:53 +0530

    Add files via upload

---
+++ Interior Designer/src/temp.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Interior Designer</title>
+    <link rel="shortcut icon" href="">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="index.css">
+    <style>
+        body{
+            padding-top:50px;
+        }
+        .starter-template{
+            padding:40px 15px;
+            text-align:center;
+        }
+        .dropdown-menu>li>a:link{
+            color:#fff; 
+        }
+        .dropdown-menu>li>a:visited{
+            color:red;
+        }
+        .dropdown-menu>li>a:hover{
+            color:#333;
+        }
+    </style>
+
+    <!--[if IE]>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+   <? include 'header.php';?>
+    
+    <div>
+             <h1>Hello</h1>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
